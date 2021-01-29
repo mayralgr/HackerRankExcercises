640e38f9d2f5cd6191343bc63cc26a7164ca23ae
mayralgr <ml17ft@gmail.com>
2021-01-28 21:07:49 -0600

    Add min max in greddy algorithms

---
+++ JS/greddy/maxmin.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the maxMin function below.
+function maxMin(k, arr) {
+    arr.sort((a,b) => a-b);
+    let min = Number.MAX_VALUE;
+    let start = 0;
+    let end = k - 1;
+    while( end < arr.length ) {
+        let minValue = arr[start];
+        let maxValue = arr[end];
+        min = Math.min(maxValue - minValue, min);
+        start+=1;
+        end+=1;
+    }
+    return min;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const k = parseInt(readLine(), 10);
+
+    let arr = [];
+
+    for (let i = 0; i < n; i++) {
+        const arrItem = parseInt(readLine(), 10);
+        arr.push(arrItem);
+    }
+
+    const result = maxMin(k, arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
